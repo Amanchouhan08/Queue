1df35bf7e9a6529c297b803bcfa68742a5459097
Aman Chouhan <ak6927887@gmail.com>
2025-11-07 22:17:00 +0530

    Update p01.cpp

--- p01.cpp
+++ p01.cpp
@@ -1,2 +1,32 @@
 //program to demonstrate a Queue using the STL.
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;  // create a queue of integers
+
+    // Insert elements (enqueue)
+    q.push(10);
+    q.push(20);
+    q.push(30);
+
+    cout << "Front element: " << q.front() << endl;
+    cout << "Rear element: " << q.back() << endl;
+
+    // Remove one element (dequeue)
+    q.pop();
+
+    cout << "After one pop, front element: " << q.front() << endl;
+
+    // Display all elements
+    cout << "Remaining queue elements: ";
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
 
